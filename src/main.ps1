9983f7fbd39609a0a15fc753e3a5e188a2b8ee45
milaq <micha.laqua@gmail.com>
2022-01-02 16:22:46 +0100

    update for ZeroTier 1.8.0 and later

--- src/main.ps1
+++ src/main.ps1
@@ -1,5 +1,5 @@
 $zerotierPath = (Get-ItemProperty -path 'HKLM:\SOFTWARE\Wow6432Node\ZeroTier, Inc.\ZeroTier One').Path
-$zerotierExecutable = $zerotierPath + "ZeroTier One.exe"
+$zerotierExecutable = $zerotierPath + "zerotier_desktop_ui.exe"
 $service = "ZeroTierOneService"
 
 Set-Service -Name $service -StartupType Manual
